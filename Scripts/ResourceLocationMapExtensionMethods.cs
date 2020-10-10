44cef3aab6ad701818d4a496868075c021816284
baba_s <shotababa1206@gmail.com>
2020-10-10 13:41:19 +0900

    first commit

---
+++ Scripts/ResourceLocationMapExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+namespace Kogane
+{
+	/// <summary>
+	/// ResourceLocationMap 型の拡張メソッドを管理するクラス
+	/// </summary>
+	public static class ResourceLocationMapExt
+	{
+		//================================================================================
+		// 関数(static)
+		//================================================================================
+		/// <summary>
+		/// すべての IAssetBundleResource を返します
+		/// </summary>
+		public static IEnumerable<IResourceLocation> GetAllAssetBundleLocation
+		(
+			this ResourceLocationMap self
+		)
+		{
+			var resourceType = typeof( IAssetBundleResource );
+
+			return self.Locations.Values
+					.SelectMany( x => x )
+					.Where( x => x.ResourceType == resourceType )
+					.GroupBy( x => x.PrimaryKey )
+					.Select( x => x.FirstOrDefault() )
+				;
+		}
+	}
+}
